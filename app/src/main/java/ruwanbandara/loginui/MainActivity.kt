78d8d70a4cfe64f670d70fd11ad6a0e993265d91
ruwanbandara <ruwan.se.uok@gmail.com>
2019-10-20 16:41:31 +0530

    next activity

--- app/src/main/java/ruwanbandara/loginui/MainActivity.kt
+++ app/src/main/java/ruwanbandara/loginui/MainActivity.kt
@@ -1,5 +1,6 @@
 package ruwanbandara.loginui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,7 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            
+            val intent = Intent(this,login::class.java)
+            startActivity(intent)
         }
     }
 }
